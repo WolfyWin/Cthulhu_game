cb8e285e265d98912e9e6ff74c2f2382740ee6d7
Rébecca Tinchon <rebecca.tinchon@hotmail.fr>
2023-04-27 14:48:57 +0200

    listener

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -7,8 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 class PlayerController extends AbstractController
